643df7332b58d180df377af533df4ca96f791598
James Bonner <jbonner89@gmail.com>
2017-09-05 15:14:02 -0700

    make enemies move randomly across the board every second

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,161 @@
 // start slingin' some d3 here.
+// var jsonCircles = [
+//   { "x_axis": 30, "y_axis": 30, "radius": 6},    //MAKE X_AXIS RANDOM
+//   { "x_axis": 70, "y_axis": 70, "radius": 6},    //MAKE Y_AXIS RANDOM
+//   { "x_axis": 110, "y_axis": 100, "radius": 6}];
+
+// var svgContainer = d3.select("body").append("svg")
+//                                     .attr("width", 200)
+//                                     .attr("height", 200);
+
+// var circles = svgContainer.selectAll("circle")
+//                           .data(jsonCircles)
+//                           .enter()
+//                           .append("circle");
+
+// var circleAttributes = circles
+//                        .attr("cx", function (d) { return d.x_axis; })
+//                        .attr("cy", function (d) { return d.y_axis; })
+//                        .attr("r", function (d) { return d.radius; });
+
+var gameOptions = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20
+};
+
+var gameStats = {
+  score: 0,
+  bestScore: 0
+};
+
+var axes = {
+  x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.width]),
+  y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
+};
+
+var gameBoard = d3.select('.board')
+  .append('svg:svg')
+  .attr('width', gameOptions.width)
+  .attr('height', gameOptions.height);
+
+var createEnemies = function() {
+  return _.range(0, gameOptions.nEnemies).map(function(i) {
+    return {
+      id: i,
+      x: Math.random() * 100,
+      y: Math.random() * 100
+    };
+  });
+};
+
+var render = function(enemy_data) {
+  var enemies = gameBoard.selectAll('circle.enemy').data(enemy_data, function(d) {
+    return d.id;
+  });
+  enemies.enter().append('svg:circle')
+    .attr('class', 'enemy')
+    .attr('cx', function(enemy) {
+      return axes.x(enemy.x);
+    }).attr('cy', function(enemy) {
+      return axes.y(enemy.y);
+    }).attr('r', 0);
+
+  //WHY DO WE NEED THIS IF WE'RE NOT ADDING DATA LIKE TAXI EXAMPLE?
+  enemies.exit().remove();
+
+  // var checkCollision = function() {
+
+  // };
+
+  // var onCollision = function() {
+
+  // };
+
+  // var tweenWithCollisionDetection = function() {
+
+  // };
+
+  // enemies.move = function(new_position_data) {
+  //   return this.transition().duration(1000)
+  //     .attr('cx', function(enemy) {
+  //       return axes.x(enemy)
+  //     })
+  // };
+
+
+  return enemies.transition().duration(1000)
+    .attr('r', 10)
+    .transition().duration(1000)
+    .attr('cx', function(enemy) {
+      return Math.random() * 700;
+    }).attr('cy', function(enemy) {
+      return Math.random() * 450;
+    })
+};
+
+var move = function(enemy_data) {
+
+
+}
+
+// var newEnemyPositions = createEnemies();
+// render(newEnemyPositions);
+
+
+var play = function() {
+  // var newEnemies = createEnemies();
+  // var enemies = render(newEnemyPositions);
+
+  var gameTurn = function() {
+    var newEnemyPositions = createEnemies();
+    return render(newEnemyPositions);
+  }
+  gameTurn();
+  setInterval(gameTurn, 2000);
+}
+
+play();
+
+
+
+
+
+
+// LOOP PART OF THE GAME
+// var play = function() {
+
+//   var gameTurn = function() {
+//     //create enemies
+//     var newEnemyPositions = createEnemies();
+//     //render enemies
+//     return render(newEnemyPositions);
+//   }
+//   var increaseScore = function() {
+
+//   }
+//   gameTurn();
+//   setInterval.bind(this, gameTurn, 2000);
+//   return setInterval.bind(this, increaseScore, 50);
+// }
+
+// play();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
